bcc98c07aa65782d89545acbee3f90022b4cc9b2
Iñigo Montánchez <mci.m89@gmail.com>
2025-09-23 00:25:00 +0200

    add event to save data

--- src/commands/analizar_img.js
+++ src/commands/analizar_img.js
@@ -15,7 +15,7 @@ module.exports = {
                 .setRequired(false)
         ),
     async execute(interaction) {
-        await interaction.deferReply();
+        // await interaction.deferReply();
 
         const member = interaction.options.getMember("usuario") || interaction.member;
 
