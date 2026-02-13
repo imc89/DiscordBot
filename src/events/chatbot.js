0d9ce034af2874d6015b2c88614e4c352f183e81
Iñigo Montánchez <mci.m89@gmail.com>
2026-02-13 12:43:11 +0100

    add new rules to chatbot prompt

--- src/events/chatbot.js
+++ src/events/chatbot.js
@@ -5,7 +5,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Configuración de Gemini
 // ========================
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemma-3-12b-it" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-tts" });
 
 // ========================
 // Almacenamiento Temporal del Historial de Chat
